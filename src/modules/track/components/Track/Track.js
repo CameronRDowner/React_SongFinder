610708b35043c7632c5ff95fb6eb2fb4d7596e08
Cameron Downer <camerondowner@gmail.com>
2020-09-06 14:51:57 -0500

    styled TracksContainer and Track adjusted Track markup

--- src/modules/track/components/Track/Track.js
+++ src/modules/track/components/Track/Track.js
@@ -5,9 +5,9 @@ export const Track = (properties) => {
     const {track} = properties;
     return (
         <div className={styles.wrapper}>
-           <h5>{track.artist_name}</h5>
-            <h6>{track.track_name}</h6>
-            <h6>{track.album_name}</h6>
+            <div className={styles['property-wrapper']}><i className={`${styles.icon} fas fa-play`}></i><span className={styles.text}>{track.track_name}</span></div>
+            <div className={styles['property-wrapper']}><i className={`${styles.icon} fas fa-record-vinyl`}></i><span className={styles.text}>{track.album_name}</span></div>
+            <div className={styles['property-wrapper']}><i className={`${styles.icon} fas fa-user`}></i><span className={styles.text}>{track.artist_name}</span></div>
             <Link to={`lyrics/track/${track.track_id}`}>
             <span>View Lyrics</span>
         </Link>

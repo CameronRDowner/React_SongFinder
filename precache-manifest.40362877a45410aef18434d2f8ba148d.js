d3b8cc433475c6b31d4b4d04f3a644d9b06b9f27
Cameron Downer <camerondowner@gmail.com>
2020-09-06 20:46:56 -0500

    Updates

---
+++ precache-manifest.40362877a45410aef18434d2f8ba148d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a33fa5b66abfeedbf93a5a4e531a421",
+    "url": "/React_SongHunter/index.html"
+  },
+  {
+    "revision": "81a584f81cd059a84634",
+    "url": "/React_SongHunter/static/css/main.0ec8c8be.chunk.css"
+  },
+  {
+    "revision": "c67dce4cd54ce8eb71ae",
+    "url": "/React_SongHunter/static/js/2.219aea35.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/React_SongHunter/static/js/2.219aea35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a584f81cd059a84634",
+    "url": "/React_SongHunter/static/js/main.952e0bed.chunk.js"
+  },
+  {
+    "revision": "9c1800d3558babeb7b76",
+    "url": "/React_SongHunter/static/js/runtime-main.15c1e344.js"
+  }
+]);
